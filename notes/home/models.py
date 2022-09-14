7ac347ed44faccf4c6db3a4429e157f624e57bb8
Fatimah Mohammed <fmg1413@hotmail.com>
2022-09-14 05:42:48 +0300

    change max length of title

--- notes/home/models.py
+++ notes/home/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 
 class Notes(models.Model):
-    title = models.CharField(max_length=2024)
+    title = models.CharField(max_length=2048)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
